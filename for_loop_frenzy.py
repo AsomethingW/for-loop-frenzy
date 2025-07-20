184d97a15c91edf8ee69dd79039c9d915258816b
AsomethingW <albert.wakuteka@hgs.hiddengeniusproject.org>
2025-07-20 14:31:32 -0400

    Update for_loop_frenzy.py

--- for_loop_frenzy.py
+++ for_loop_frenzy.py
@@ -1,7 +1,7 @@
 for i in range(1,11):
     print(i)
 
-favorite_artist = ["Steve Lacy", "Old Kanye", "SWV", "TTT", "Sade"]
+favorite_artist = ["Steve Lacy", "Kanye West", "SWV", "TTT", "Sade"]
 
 for x in favorite_artist:
     print(x)
